e3b65873599a71f81596e09bdf39825057777b38
AD-milk <619656137@qq.com>
2022-02-03 22:30:43 +0800

    基于TCP的缓存服务

--- server/main.go
+++ server/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"cache-server/server/cache"
 	"cache-server/server/http"
+	"cache-server/server/tcp"
 )
 
 func main() {
 	c := cache.New("inmemory")
+	go tcp.New(c).Listen() // add tcp service
 	http.New(c).Listen()
 }

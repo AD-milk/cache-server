a0b74d7875bae9e1c93e3a3f73eb840213748878
AD-milk <619656137@qq.com>
2022-02-05 23:23:24 +0800

    基于rocksdb现持久化

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module cache-server
 
 go 1.17
 
-require github.com/go-redis/redis v6.15.9+incompatible // indirect
+require (
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	stathat.com/c/consistent v1.0.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -4,10 +4,15 @@ import (
 	"cache-server/server/cache"
 	"cache-server/server/http"
 	"cache-server/server/tcp"
+	"flag"
+	"log"
 )
 
 func main() {
-	c := cache.New("inmemory")
+	typ := flag.String("type", "inmemory", "cache type")
+	flag.Parse()
+	log.Println("type is: ", *typ)
+	c := cache.New(*typ)
 	go tcp.New(c).Listen() // add tcp service
 	http.New(c).Listen()
 }

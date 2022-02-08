c7397a77db0e3ed2f9224ad5a0ce5e2d951ed0ed
AD-milk <619656137@qq.com>
2022-02-08 17:08:39 +0800

    rebalance

--- server/http/server.go
+++ server/http/server.go
@@ -15,7 +15,8 @@ func (s *Server) Listen() {
 	http.Handle("/cache/", s.cacheHandler())
 	http.Handle("/status", s.statusHandler())
 	http.Handle("/cluster", s.clusterHandler())
-	http.ListenAndServe(":12345", nil)
+	http.Handle("/rebalance", s.rebalanceHandler())
+	http.ListenAndServe(s.Addr()+":12345", nil)
 }
 
 func New(c cache.Cache, n cluster.Node) *Server {
